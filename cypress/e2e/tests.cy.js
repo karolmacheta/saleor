d50cc78d911312f1c73783bdd1449aa7c3630cc0
karolmacheta <karol.macheta@gmail.com>
2022-10-25 17:34:06 +0200

    refactor + cypress 10

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -15,16 +15,13 @@ describe("Test for changing login pass", () => {
         cy.get(adminPage.userSettingsBtn).should('be.visible').click();
         cy.url().should('include', 'dashboard/staff/');
        cy.get(adminPage.changePassBtn).click();
-
     });
 
     it("Negative Scenario - Cant change pass in demo mode", () => {
         cy.get(adminPage.oldPassInput).clear().type('admin');
         cy.get(adminPage.newPassInput).clear().type('123456789');
         cy.get(adminPage.saveBtn).click();
-        cy.get(adminPage.notification).should('be.visible').invoke("text").then(text => {
-            expect(text).to.contain("Saleor runs in read-only mode. Changes not saved.");
-        })
+        cy.checkNotificationText("Saleor runs in read-only mode. Changes not saved.");
     });
 
     it("Negative Scenario - Cant change pass in demo mode with mocked data", () => {
@@ -36,16 +33,8 @@ describe("Test for changing login pass", () => {
         }).as('mockedNegativeResponse');
 
         cy.get(adminPage.saveBtn).click();
-
-        cy.get("@mockedNegativeResponse").then(res => {
-            console.log(res)
-            expect(res.response.statusCode).to.equal(200)
-        });
-
-        cy.get('[data-test="notification"]').should('be.visible').invoke("text").then(text => {
-            cy.log(text);
-            expect(text).to.contain("Old password isn't valid.See error log");
-        });
+        cy.expectResponseStatus("@mockedNegativeResponse", 200);
+        cy.checkNotificationText("Old password isn't valid.See error log");
     });
 
     it("Positive Scenario - Can change pass in demo mode with mocked data", () => {
@@ -57,15 +46,7 @@ describe("Test for changing login pass", () => {
         }).as('mockedPositiveResponse');
 
         cy.get(adminPage.saveBtn).click();
-
-        cy.get("@mockedPositiveResponse").then(res => {
-            console.log(res)
-            expect(res.response.statusCode).to.equal(200)
-        });
-
-        cy.get('[data-test="notification"]').should('be.visible').invoke("text").then(text => {
-            cy.log(text);
-            expect(text).to.contain("Saved changes");
-        });
+        cy.expectResponseStatus("@mockedPositiveResponse", 200);
+        cy.checkNotificationText("Saved changes");
     });
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,3 +5,19 @@ Cypress.Commands.add('login', (email, password) => {
     cy.get(adminPage.loginForm.pass).clear().type(password);
     cy.get(adminPage.loginForm.submitBtn).click();
 })
+
+Cypress.Commands.add('checkNotificationText', (notificationText) => {
+
+cy.get(adminPage.notification).should('be.visible').invoke("text").then(text => {
+    cy.log(text);
+    expect(text).to.contain(notificationText);
+    });
+})
+
+Cypress.Commands.add('expectResponseStatus', (apiCall, code) => {
+    cy.get(apiCall).then(res => {
+        console.log(res)
+        expect(res.response.statusCode).to.equal(code)
+    });
+});
+
